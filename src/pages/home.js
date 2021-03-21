710ff43d85c6774dec3878db16c8d2e8110cca99
Travisse Hansen <travisseh@gmail.com>
2021-03-21 07:37:59 +0000

    added responsive modal for adding emotions

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,12 +7,24 @@ import '../App.css'
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const  Home = () => {
   const history = useHistory()
+  const theme = useTheme()
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const [emotions, setEmotions] = useState([])
   const [inputValue, setInputValue] = useState('')
+  const [modalOpen, setModalOpen] = useState(false)
 
   console.log("emotions", emotions)
 
@@ -82,7 +94,10 @@ const  Home = () => {
         <div id="header">Emotions</div>
         <div>
         <Link to="/login" style={{marginRight: 16}}> Go to Login</Link>
-          <Button onClick={() => logout()} variant="contained" color="primary">Logout</Button>
+        <Button onClick={() => logout()} variant="contained" color="primary">Logout</Button>
+        <div>
+          <Button variant="outlined" color="primary" onClick={() => setModalOpen(true)}>Add Emotion </Button>
+        </div>
         </div>
         <Input/>
         <div id="input" style={{marginTop: 16}}>
@@ -97,7 +112,31 @@ const  Home = () => {
         </div>
       )}
      
-        
+        <Dialog fullScreen={fullScreen} open={modalOpen} onClose={() => setModalOpen(false)} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setModalOpen(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={() => setModalOpen(false)} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
       </div>
     );
 }
